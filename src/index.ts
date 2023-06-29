67c32c226401d99f43cff36251cedc1353cc07ad
olegpl1993 <olegpl1993@gmail.com>
2023-06-29 20:51:53 +0300

    feat: base ts lint config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-const message: string = 'Hello, TypeScript!';
+const func = (str: string, str2: string) => str + str2;
+
+const message = 'Hello, TypeScript!';
 console.log(message);
+
+const message2 = 'Hello, TypeScript!';
+console.log(message2);
+
+const message3 = func(message, message2);
+console.log(message3);
