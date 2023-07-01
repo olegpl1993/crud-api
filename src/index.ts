0891b8c3d5a86f68d8dc36142e2558d1d4a34035
olegpl1993 <olegpl1993@gmail.com>
2023-07-01 23:28:47 +0300

    feat: install uuid types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,49 @@
-const func = (str: string, str2: string) => str + str2;
+import http, { IncomingMessage, ServerResponse } from 'http';
 
-const message = 'Hello, TypeScript!';
-console.log(message);
+// import uuid from 'uuid';
+// const id = uuid.v4();
+// console.log(id);
 
-const message2 = 'Hello, TypeScript!';
-console.log(message2);
+// Интерфейс пользователя
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  hobbies: string[];
+}
 
-const message3 = func(message, message2);
-console.log(message3);
+// Массив для хранения пользователей
+const users: User[] = [];
+
+// Создаем сервер
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    if (req.method === 'GET' && req.url === '/users') {
+      // Чтение списка пользователей
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(users));
+    } else if (req.method === 'POST' && req.url === '/users') {
+      // Создание нового пользователя
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', () => {
+        const newUser: User = JSON.parse(body);
+        users.push(newUser);
+
+        res.statusCode = 201;
+        res.end('User created');
+      });
+    } else {
+      res.statusCode = 404;
+      res.end('Not Found');
+    }
+  },
+);
+
+// Запускаем сервер на порте 3000
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
